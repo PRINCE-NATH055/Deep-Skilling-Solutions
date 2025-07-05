01d060daa36b39e7598ca9386fdc08b9dfbbcd4a
PRINCE-NATH055 <princenath055@gmail.com>
2025-07-06 00:22:35 +0530

    Week 3 done

---
+++ 6361909_Week3/6361909_Week3_Entity_Framework_Core_8.0/EF_Core_8.0_HOL_Lab2/Code/Lab2_DbContextSetup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RetailInventory.Labs
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public List<Product> Products { get; set; } = new();
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+    }
+
+
+
+    public class AppDbContext : DbContext
+    {
+        
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=BT-22051178\\SQLEXPRESS;Database=RetailStore;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;");
+        }
+
+
+    }
+
+    public static class Lab2_DbContextSetup
+    {
+        public static void Run()
+        {
+            using var context = new AppDbContext();
+            context.Database.EnsureCreated();
+            Console.WriteLine("Lab 2 - DbContext setup complete. Database ensured.");
+        }
+    }
+}
