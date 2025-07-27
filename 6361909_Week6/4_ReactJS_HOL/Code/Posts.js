cc7abf7ec5789a7074b6728f3986bb888a60b26e
PRINCE-NATH055 <princenath055@gmail.com>
2025-07-27 22:25:13 +0530

    Week 6 done

---
+++ 6361909_Week6/4_ReactJS_HOL/Code/Posts.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Post from './Post';
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false
+    };
+  }
+
+  loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => {
+        const postList = data.map(post => new Post(post.id, post.title, post.body));
+        this.setState({ posts: postList });
+      })
+      .catch(error => {
+        console.error("Error loading posts:", error);
+        this.setState({ hasError: true });
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    alert('An error occurred in Posts component');
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong.</h2>;
+    }
+
+    return (
+      <div>
+        <h2>All Posts</h2>
+        {this.state.posts.map(post => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
